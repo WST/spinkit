9e39bcf6b63a5b50f7000258ed127e34f6345829
WST <ilya.averkov@gmail.com>
2016-04-26 17:28:52 +0300

    Example app

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+use SpinDash\Application;
+
+final class Spinkit extends Application
+{
+	public function setupRoutes() {
+		$this->get('/', [$this, 'homePage']);
+	}
+
+	public function setupDynamicRoutes($request) {
+
+	}
+
+	public function homePage($request, $response) {
+		$response->setBody('Hello, world!');
+	}
+}
+
+$app = new Spinkit("config.php");
+$app->run();
